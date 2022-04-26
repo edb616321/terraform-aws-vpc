2f9c9c4886beb77e2db07a6d0c3c89af001ac656
edb616321 <97626015+edb616321@users.noreply.github.com>
2022-04-26 14:25:26 -0700

    modifing providers section and re-ran terraform fmt

--- main.tf
+++ main.tf
@@ -1,8 +1,14 @@
 terraform {
-  provider "aws" {
-    profile = "default"
-    region  = "us-west-2"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
   }
+}
+
+provider "aws" {
+  region = "us-west-2"
 }
 
 resource "aws_vpc" "main" {
